a25f92962f4edb8c8e36f1e52475d1ff5da400db
Kipp Cannon <kipp.cannon@ligo.org>
2021-08-23 17:42:47 +0900

    docs:  try fixing AttributeError
    - see https://github.com/sphinx-doc/sphinx/issues/7747

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,7 @@ def setup_static_content(app):
 
         # add stylesheets
         for cssf in glob.glob(os.path.join(staticdir, 'css', '*.css')):
-            app.add_stylesheet(cssf[len(staticdir)+1:])
+            app.add_css_file(cssf[len(staticdir)+1:])
 
         # add custom javascript
         for jsf in glob.glob(os.path.join(staticdir, 'js', '*.js')):
